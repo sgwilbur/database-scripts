cbfcb796275323c6dd0eae5a1cb112a5c3f7b516
Sean G Wilbur <sgwilbur@gmail.com>
2011-04-01 15:14:41 -0500

    Removed -p option from user add script, because I was using it wrong.

--- db2/add_db_user.sh
+++ db2/add_db_user.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 
-/usr/sbin/useradd -r -d /home/db2inst1 -s /sbin/nologin -p ${2} ${1}
+/usr/sbin/useradd -r -d /home/db2inst1 -s /sbin/nologin ${1}
+
+echo "Don't forget to set this user's password now!"
