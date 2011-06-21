038c639fd924cf4d5cb7b5d5642a9c7956bc045a
Sean G Wilbur <sgwilbur@gmail.com>
2011-06-21 00:01:40 -0500

    Updating pagesize and default to give dbadm

--- db2/add_db.sh
+++ db2/add_db.sh
@@ -5,11 +5,11 @@
 db2inst_home=/home/db2inst1
 codeset=UTF-8
 territory=en
-pagesize=8192
+pagesize=16384
 
 db2 create database $1 on ${db2inst_home} using codeset ${codeset} territory ${territory} pagesize ${pagesize}
 db2 connect to $1
-#db2 grant dbadm on database to user $2
+db2 grant dbadm on database to user $2
 #db2 revoke connect on database from public
 
 db2 create schema $2
